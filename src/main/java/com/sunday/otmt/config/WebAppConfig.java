a769dbae83fea717e14ea7b3dd511c74fc878633
tosyaliselim <selimtosyali@protonmail.com>
2021-01-23 23:31:27 +0300

    Initial commit

---
+++ src/main/java/com/sunday/otmt/config/WebAppConfig.java
@@ -0,0 +1,27 @@
+package com.sunday.otmt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages="com.sunday.otmt")
+public class WebAppConfig {
+	
+	@Bean
+	public ViewResolver viewResolver() {
+		
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		
+		viewResolver.setPrefix("/WEB-INF/view/");
+		viewResolver.setSuffix(".jsp");
+		
+		return viewResolver;
+		
+	}
+	
+}
