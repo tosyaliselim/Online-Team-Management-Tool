dca0c0c29750a057c76b6ba5a31e0d74fdda3606
tosyaliselim <selimtosyali@protonmail.com>
2021-01-26 10:39:33 +0300

    HomeController was updated.

--- src/main/java/com/sunday/otmt/controller/HomeController.java
+++ src/main/java/com/sunday/otmt/controller/HomeController.java
@@ -1,13 +1,39 @@
 package com.sunday.otmt.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.sunday.otmt.entity.User;
+import com.sunday.otmt.service.GenericService;
 
 @Controller
 public class HomeController {
 	
+	@Autowired
+	@Qualifier("userServiceImpl")
+	private GenericService<User> userService;
+	
 	@GetMapping("/")
-	public String showHomePage() {
+	public String showHomePage(HttpServletRequest req) {
+		
+		HttpSession session = req.getSession();
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+	    String currentUserName = authentication.getName();
+	    User user = null;
+	    try {
+		    user = userService.getByName(currentUserName);
+	    } catch (Exception e) {
+			return "error-page";
+		}
+	    session.setAttribute("currentUser", user);
+		
 		return "home";
 	}
 }
--- src/main/java/com/sunday/otmt/controller/TeamController.java
+++ src/main/java/com/sunday/otmt/controller/TeamController.java
@@ -1,12 +1,7 @@
 package com.sunday.otmt.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,22 +26,7 @@ public class TeamController {
 	private GenericService<User> userService;
 
 	@GetMapping("/showForm")
-	public String showCreateTeamForm(Model model, HttpServletRequest req) {
-		
-		HttpSession session = req.getSession();
-		
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-	    String currentUserName = authentication.getName();
-	    
-	    User user = null;
-	    try {
-		    user = userService.getByName(currentUserName);
-	    } catch (Exception e) {
-			return "error-page";
-		}
-	    
-	    session.setAttribute("currentUser", user);
-		
+	public String showForm(Model model) {
 		model.addAttribute("team", new Team());
 
 		return "create-team-form";
@@ -54,6 +34,7 @@ public class TeamController {
 
 	@PostMapping("/createTeam")
 	public String createTeam(@ModelAttribute("team") Team team) {
+		
 		teamService.save(team);
 		return "redirect:/team/all";
 	}
