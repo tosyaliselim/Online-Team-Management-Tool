c695de1fe46adb3d709553f4df70e671c7545ea5
tosyaliselim <selimtosyali@protonmail.com>
2021-01-31 23:41:49 +0300

    Entities were updated.

--- src/main/java/com/sunday/otmt/entity/Project.java
+++ src/main/java/com/sunday/otmt/entity/Project.java
@@ -6,6 +6,10 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.persistence.*;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 
 @Getter
 @Setter
@@ -14,16 +18,47 @@ import java.util.List;
 @AllArgsConstructor
 public class Project {
 
-	// TODO: Make it an Hibernate Entity
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ProjectId")
 	private int id;
+	
+	@Column(name = "Name")
 	private String name;
+	
+	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern = "yyyy/MM/dd")
+	@Column(name = "StartDate")
 	private Date startDate;
+	
+	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern = "yyyy/MM/dd")
+	@Column(name = "EndDate")
 	private Date endDate;
+	
+	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern = "yyyy/MM/dd")
+	@Column(name = "CreatedAt")
 	private Date createdAt;
+	
+	@Column(name = "Description")
 	private String description;
+	
+	@ManyToOne(fetch = FetchType.EAGER, cascade = {
+			CascadeType.MERGE, CascadeType.DETACH, CascadeType.PERSIST, CascadeType.REFRESH
+	})
+	@JoinColumn(name = "ProjectManagerId")
 	private User manager;
+	
+	@ManyToOne(fetch = FetchType.EAGER, cascade = {
+			CascadeType.MERGE, CascadeType.DETACH, CascadeType.PERSIST, CascadeType.REFRESH
+	})
+	@JoinColumn(name = "OwnerTeam")
 	private Team ownerTeam;
+	
+	@OneToMany(fetch = FetchType.LAZY, cascade = {
+			CascadeType.MERGE, CascadeType.DETACH, CascadeType.PERSIST, CascadeType.REFRESH
+	}, mappedBy = "ownerProject")
 	private List<Task> projectTasks;
 
 	public void addTask(Task task) {
